642f23a70ec8b5a242b8b388855cd08087750dcf
p.konstantinov <dzhangitau@gmail.com>
2023-11-20 12:38:08 +0400

    Remove redundant dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,9 +55,6 @@ dependencies {
     test()
     navigation()
     hilt()
-    retrofit()
-    compose(platform("androidx.compose:compose-bom:2023.03.00"))
-    coroutines()
 
     implementation(project(mapOf("path" to ":feature:splash")))
     implementation(project(mapOf("path" to ":feature:login")))
